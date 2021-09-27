6c00053e59eb33224e03a6d4ee91d86032d2f84e
[uli3120] <[albertoulises98@gmail.com]>
2021-09-27 18:07:13 -0500

    first commit

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ItemCount from './ItemCount';
+
+
+const ItemListContainer=({mensaje})=> {
+    return (
+        <div>
+            <ItemCount/>
+        </div>
+    )
+}
+export default ItemListContainer;
